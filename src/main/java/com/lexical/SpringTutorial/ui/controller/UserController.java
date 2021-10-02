bc5f661fa8352119885b907980a118de1b7883f2
ian lombu <citrakemurnian@gmail.com>
2021-10-02 23:50:02 +0700

    Exceptions Handling

--- src/main/java/com/lexical/SpringTutorial/ui/controller/UserController.java
+++ src/main/java/com/lexical/SpringTutorial/ui/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.lexical.SpringTutorial.ui.controller;
 
+import com.lexical.SpringTutorial.exceptions.UserServiceException;
 import com.lexical.SpringTutorial.service.UserService;
 import com.lexical.SpringTutorial.shared.dto.UserDto;
 import com.lexical.SpringTutorial.ui.model.request.UserDetailsRequestModel;
+import com.lexical.SpringTutorial.ui.model.response.ErrorMessages;
 import com.lexical.SpringTutorial.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +28,11 @@ public class UserController {
     }
 
     @PostMapping
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
         UserRest returnValue = new UserRest();
+
+//        if (userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+        if (userDetails.getFirstName().isEmpty()) throw new NullPointerException("The object is null");
 
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails, userDto);
